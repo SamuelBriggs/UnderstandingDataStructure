ffa9801174ffbf6184cab56464113fae9db630b2
Alawode Samuel <tolulope.alawode@gmail.com>
2023-04-22 19:29:08 +0100

    new stuff

---
+++ src/LinkedList/ImplementationAndOperations/DoublyLinkedList.java
@@ -0,0 +1,15 @@
+package LinkedList.ImplementationAndOperations;
+
+import java.util.LinkedList;
+
+
+public class DoublyLinkedList {
+
+    static class Node {
+        private Object value;
+        private Node previousNode;
+        private Node nextNode;
+
+
+    }
+}
